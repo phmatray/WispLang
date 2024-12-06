a9d76b1b7e7e482b9a1216f58383a39d9954fb8c
Philippe Matray <phmatray@gmail.com>
2024-12-06 15:58:00 +0100

    create AST models

---
+++ AstPrinter/Program.cs
@@ -0,0 +1,16 @@
+﻿using WispScanner;
+
+Console.WriteLine("AstPrinter");
+
+Expr expression = new Expr.Binary(
+    new Expr.Unary(
+        new Token(TokenType.MINUS, "-", null, 1),
+        new Expr.Literal(123)
+    ),
+    new Token(TokenType.STAR, "*", null, 1),
+    new Expr.Grouping(
+        new Expr.Literal(45.67)
+    )
+);
+
+Console.WriteLine(new AstPrinter().Print(expression));
